5975018bb37c4efebdc6a5d95972325798971270
1U23CA059MILTONRAJA <139437416+1U23CA059MILTONRAJA@users.noreply.github.com>
2024-01-20 04:36:46 +0000

    don

--- Person.java
+++ Person.java
@@ -15,8 +15,8 @@ public class Person {
     public static void main (String args[]){
         person1 a1=new person1 ("Jhon","Doe",30);
         person1 a2=new person1 ("Alice","Smith",25);
-        System.out.println("person1:"+a1.getfullname());
-        System.out.println("person2:"+a2.getfullname());
+        System.out.println("person 1:"+a1.getfullname());
+        System.out.println("person 2:"+a2.getfullname());
         double b= (a1.age+a2.age)/2;//
         System.out.println("Average Age:"+b);
 
